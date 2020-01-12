d0e1539a6551da5ee8100ed4971e6b2e7b0cc7b6
Martin Refseth <20230450+HDR@users.noreply.github.com>
2020-01-12 23:39:33 +0100

    Update serial.js

--- docs/serial.js
+++ docs/serial.js
@@ -11,20 +11,7 @@ var serial = {};
 
   serial.requestPort = function() {
     const filters = [
-      { 'vendorId': 0x2341, 'productId': 0x8036 }, // Arduino Leonardo
-      { 'vendorId': 0x2341, 'productId': 0x8037 }, // Arduino Micro
-      { 'vendorId': 0x2341, 'productId': 0x804d }, // Arduino/Genuino Zero
-      { 'vendorId': 0x2341, 'productId': 0x804e }, // Arduino/Genuino MKR1000
-      { 'vendorId': 0x2341, 'productId': 0x804f }, // Arduino MKRZERO
-      { 'vendorId': 0x2341, 'productId': 0x8050 }, // Arduino MKR FOX 1200
-      { 'vendorId': 0x2341, 'productId': 0x8052 }, // Arduino MKR GSM 1400
-      { 'vendorId': 0x2341, 'productId': 0x8053 }, // Arduino MKR WAN 1300
-      { 'vendorId': 0x2341, 'productId': 0x8054 }, // Arduino MKR WiFi 1010
-      { 'vendorId': 0x2341, 'productId': 0x8055 }, // Arduino MKR NB 1500
-      { 'vendorId': 0x2341, 'productId': 0x8056 }, // Arduino MKR Vidor 4000
-      { 'vendorId': 0x2341, 'productId': 0x8057 }, // Arduino NANO 33 IoT
       { 'vendorId': 0x046d }, //JoeyGen3
-      { 'vendorId': 0x239A }, // Adafruit Boards
     ];
     return navigator.usb.requestDevice({ 'filters': filters }).then(
       device => new serial.Port(device)
@@ -33,9 +20,9 @@ var serial = {};
 
   serial.Port = function(device) {
     this.device_ = device;
-    this.interfaceNumber_ = 2;  // original interface number of WebUSB Arduino demo
-    this.endpointIn_ = 5;       // original in endpoint ID of WebUSB Arduino demo
-    this.endpointOut_ = 4;      // original out endpoint ID of WebUSB Arduino demo
+    this.interfaceNumber_ = 2;
+    this.endpointIn_ = 5;
+    this.endpointOut_ = 4;
   };
 
   serial.Port.prototype.connect = function() {
