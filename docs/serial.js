d99b3c60cccdb3ff4369149b97476940dd141253
Martin Refseth <20230450+HDR@users.noreply.github.com>
2020-01-12 23:37:01 +0100

    Update serial.js

--- docs/serial.js
+++ docs/serial.js
@@ -23,7 +23,7 @@ var serial = {};
       { 'vendorId': 0x2341, 'productId': 0x8055 }, // Arduino MKR NB 1500
       { 'vendorId': 0x2341, 'productId': 0x8056 }, // Arduino MKR Vidor 4000
       { 'vendorId': 0x2341, 'productId': 0x8057 }, // Arduino NANO 33 IoT
-      { 'vendorId': 0x046d, 'productId': 0x1234 ], //JoeyGen3
+      { 'vendorId': 0x046d }, //JoeyGen3
       { 'vendorId': 0x239A }, // Adafruit Boards!
     ];
     return navigator.usb.requestDevice({ 'filters': filters }).then(
